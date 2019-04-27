952e0199dfb59d61d5697d6110f8b93e7b673ceb
Joe Reese <38056103+JoeReeseVT@users.noreply.github.com>
2019-04-27 18:10:38 -0400

    Update velocity.vhd

--- data_processing/velocity.vhd
+++ data_processing/velocity.vhd
@@ -13,8 +13,8 @@
  *  Author(s):
  *    Joe
  *   
- *  Version: 0.1.0
- *  Updated: 16 Apr 2019 by Joe
+ *  Version: 0.2.0
+ *  Updated: 27 Apr 2019 by Joe
  */
  
 library IEEE;
@@ -24,7 +24,8 @@ use IEEE.numeric_std.all;
 entity velocity is
   port(
 	  clk           : in  std_logic; -- We want velocity to update kinda slowly
-    u, d, l, r, f : in std_logic; 
+    u, d, l, r, f : in  std_logic; 
+		off_track     : in  std_logic; -- rgb_o(4) from the track rgb output
     vel_x, vel_y  : out signed(3 downto 0) := 4d"0"
     );
 end velocity;
@@ -37,21 +38,38 @@ begin
   process (clk) begin
 	  if rising_edge(clk) then
 		
+		
 		  /* If we let go of the direction, we want to preserve what direction we were going. */
 		  if u and (not d) then
-				peak_y <= to_signed(-7, 4);
+			  if off_track then 
+					peak_y <= to_signed(-1, 4);
+				else
+					peak_y <= to_signed(-7, 4);
+				end if;
 		  elsif d and (not u) then
-				peak_y <= to_signed(7, 4);
+				if off_track then
+					peak_y <= to_signed(1, 4);
+				else
+					peak_y <= to_signed(7, 4);
+				end if;
 			else 
-			  peak_y <= vel_y;
+  			peak_y <= vel_y;
 			end if;
 		  
 			if l and (not r) then
-				peak_x <= to_signed(-7, 4);
+			  if off_track then
+					peak_x <= to_signed(-1, 4);
+				else
+					peak_x <= to_signed(-7, 4);
+				end if;
 		  elsif r and (not l) then
-				peak_x <= to_signed(7, 4);
+				if off_track then
+					peak_x <= to_signed(1, 4);
+				else
+					peak_x <= to_signed(7, 4);
+				end if;
 			else 
-			  peak_x <= vel_x;
+				peak_x <= vel_x;
 			end if;
 		
 		  /* 
