8ecffaef4a56ce3c059a1fcc5de6c953b192250b
Joe Reese <38056103+JoeReeseVT@users.noreply.github.com>
2019-04-27 17:39:36 -0400

    Update direction_calc.vhd

--- data_processing/direction_calc.vhd
+++ data_processing/direction_calc.vhd
@@ -26,12 +26,20 @@ end;
 
 sprite code
 eight colors
-rgb<=
-			"000011" when direction = 3d"0" and vga_col > pos_col - 10d"20" and vga_col < pos_col + 10d"20" and vga_row > pos_vga - 10d"20" and vga_row < pos_vga + 10d"20" else
-			"001111" when direction = 3d"1" and vga_col > pos_col - 10d"20" and vga_col < pos_col + 10d"20" and vga_row > pos_vga - 10d"20" and vga_row < pos_vga + 10d"20" else
-			"001100" when direction = 3d"2" and vga_col > pos_col - 10d"20" and vga_col < pos_col + 10d"20" and vga_row > pos_vga - 10d"20" and vga_row < pos_vga + 10d"20" else
-			"111100" when direction = 3d"3" and vga_col > pos_col - 10d"20" and vga_col < pos_col + 10d"20" and vga_row > pos_vga - 10d"20" and vga_row < pos_vga + 10d"20" else
-			"110000" when direction = 3d"4" and vga_col > pos_col - 10d"20" and vga_col < pos_col + 10d"20" and vga_row > pos_vga - 10d"20" and vga_row < pos_vga + 10d"20" else
-			"110011" when direction = 3d"5" and vga_col > pos_col - 10d"20" and vga_col < pos_col + 10d"20" and vga_row > pos_vga - 10d"20" and vga_row < pos_vga + 10d"20" else
-			"101010" when direction = 3d"6" and vga_col > pos_col - 10d"20" and vga_col < pos_col + 10d"20" and vga_row > pos_vga - 10d"20" and vga_row < pos_vga + 10d"20" else
-			"111111";
+							 
+signal color : std_logic_vector(5 downto 0);
+							 
+color <= "000011" when direction = 3d"0" else
+	 "001111" when direction = 3d"1" else
+         "001100" when direction = 3d"2" else
+	 "111100" when direction = 3d"3" else
+         "110000" when direction = 3d"4" else
+	 "110011" when direction = 3d"5" else
+	 "101010" when direction = 3d"6" else
+	 "111111" when direction = 3d"7" else
+ 	 "000000";
+						 	 
+rgb <= color when vga_col > pos_col - 10d"20" and vga_col < pos_col + 10d"20" and vga_row > pos_vga - 10d"20" and vga_row < pos_vga + 10d"20"
+             else "000000";
+			
+			
